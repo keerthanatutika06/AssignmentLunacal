7a833e1e83204ed029f9ec0f9e2a31a35d03c7b5
keerthanatutika06 <tutikakeerthanasai@gmail.com>
2024-09-11 20:27:07 +0530

    lunacall

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import Home from './components/Home';
+import './App.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route  path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
